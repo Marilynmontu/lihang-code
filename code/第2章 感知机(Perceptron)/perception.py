ee9cb013ae31809c4c14722662984ba6f0e28473
April-Cai <chusst@163.com>
2019-02-16 19:35:46 +0800

    knn

---
+++ code/第2章 感知机(Perceptron)/perception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/21 07:07
+# @Author  : April
+# @File    : perception.py
+# @Software: PyCharm
+# @Description: Use iris data set to apply perception algorithms
+
+
+
